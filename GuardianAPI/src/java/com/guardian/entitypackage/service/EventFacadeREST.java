3b10772cfc88aa69db905df56f6eeaf0ed16e28d
Ojas <ojmhetar@gmail.com>
2016-04-10 23:28:22 -0400

    web

--- GuardianAPI/src/java/com/guardian/entitypackage/service/EventFacadeREST.java
+++ GuardianAPI/src/java/com/guardian/entitypackage/service/EventFacadeREST.java
@@ -36,14 +36,14 @@ public class EventFacadeREST extends AbstractFacade<Event> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(Event entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Event entity) {
         super.edit(entity);
     }
@@ -56,21 +56,21 @@ public class EventFacadeREST extends AbstractFacade<Event> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON})
     public Event find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Event> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Event> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
